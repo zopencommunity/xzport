e764c053d4a8dce62247c9f2c82ca020718f90f3
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:08 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'xzport'), string(name: 'DESCRIPTION', 'xzport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'xzport'), string(name: 'DESCRIPTION', 'xz is a new general-purpose, command line data compression utility, similar to gzip and bzip2.' )]
         }
 }
