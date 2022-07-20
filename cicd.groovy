53ecfbac55b9f9fd379cc96816413f401b71133e
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 13:56:07 -0400

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -7,18 +7,9 @@ node('linux')
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
                         userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/xzport.git']]])
-                ])
-
-                checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: '*/main']],
-                        doGenerateSubmoduleConfigurations: false,
-                        extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/utils.git']]])
-                ])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'xzport'), string(name: 'DESCRIPTION', 'xz is a new general-purpose, command line data compression utility, similar to gzip and bzip2.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/xzport.git'), string(name: 'PORT_DESCRIPTION', value: 'xz is a new general-purpose, command line data compression utility, similar to gzip and bzip2.' )]
         }
 }
